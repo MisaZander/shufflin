b8cd2ffd6ce82d6e950d3297b6812206b73731a8
Alexander Osborn <misterzander@gmail.com>
2019-01-13 13:23:23 -0600

    Updates

---
+++ precache-manifest.2a4d148881875adf464bbbee5b20f740.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "eeadedb20ddece4d8aaba005f7114a76",
+    "url": "/shufflin/static/media/EvilRacoon.eeadedb2.jpg"
+  },
+  {
+    "revision": "bf2b0e7000efd5a236b2",
+    "url": "/shufflin/static/css/main.3701d15f.chunk.css"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/shufflin/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "dca0eada7ffc8b258f6e",
+    "url": "/shufflin/static/js/runtime~main.dca0eada.js"
+  },
+  {
+    "revision": "0cfe8eede3d865f78f46ce4e95d5021c",
+    "url": "/shufflin/static/media/AAM.0cfe8eed.jpg"
+  },
+  {
+    "revision": "55a1bd417400c5999725d24449cef12d",
+    "url": "/shufflin/static/media/AngryDuck.55a1bd41.jpg"
+  },
+  {
+    "revision": "506f0c785949bc68239147299e5a9cfb",
+    "url": "/shufflin/static/media/ConfessionBear.506f0c78.jpg"
+  },
+  {
+    "revision": "b6c588ebb4815ba5dc28c292b5dd8661",
+    "url": "/shufflin/static/media/DisasterGirl.b6c588eb.jpg"
+  },
+  {
+    "revision": "bf2b0e7000efd5a236b2",
+    "url": "/shufflin/static/js/main.bf2b0e70.chunk.js"
+  },
+  {
+    "revision": "64796f06ae1a01fb712675f94002bbe0",
+    "url": "/shufflin/static/media/ForeverAlone.64796f06.jpg"
+  },
+  {
+    "revision": "893674d8fb174ec19051ff36025354fe",
+    "url": "/shufflin/static/media/Fry.893674d8.jpg"
+  },
+  {
+    "revision": "00c99abbc4739990b3dbe3540da42837",
+    "url": "/shufflin/static/media/MAM.00c99abb.jpg"
+  },
+  {
+    "revision": "eae9b394cbabe44a50dc1302a9ff8c6c",
+    "url": "/shufflin/static/media/Pepe.eae9b394.jpg"
+  },
+  {
+    "revision": "ea2be24b30527d6927fbe5533b7c7510",
+    "url": "/shufflin/static/media/Seal.ea2be24b.jpg"
+  },
+  {
+    "revision": "6560b98bc7fceabaaf495132c611f17a",
+    "url": "/shufflin/static/media/Steve.6560b98b.jpg"
+  },
+  {
+    "revision": "2a35acbccf13a5b38ad3534358467f5e",
+    "url": "/shufflin/static/media/SuccessKid.2a35acbc.jpg"
+  },
+  {
+    "revision": "c590c8b4c27df48a6ec83e7cbf368ec7",
+    "url": "/shufflin/index.html"
+  }
+];
